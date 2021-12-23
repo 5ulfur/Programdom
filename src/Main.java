39ea82b3958e0246795bc73d62919ef97926755f
5ulfur <simonevitto1@gmail.com>
2021-12-24 00:18:22 +0100

    Added getDateNow and getTimeNow

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,22 @@
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("Hello world!");
+        System.out.println("Hello world! " + getDateNow() + " " + getTimeNow());
+    }
+
+    private static String getDateNow(){
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        return dateTimeFormatter.format(currentDateTime);
+    }
+
+    private static String getTimeNow(){
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("HH:mm:ss");
+        LocalDateTime currentDateTime = LocalDateTime.now();
+        return dateTimeFormatter.format(currentDateTime);
     }
 
 }
