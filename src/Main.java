8fc95617e4719ef6ea281a5810b22e3bc3fb8b0b
5ulfur <simonevitto1@gmail.com>
2022-03-11 19:39:12 +0100

    Added menu

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,22 @@
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args){
-        System.out.println("Hello world! " + getDateNow() + " " + getTimeNow());
+        System.out.println("Hello world!");
+
+        Scanner scanner = new Scanner(System.in);
+        String selection;
+        do {
+            System.out.println("Programdom:\n\t1. Date now\n\t2. Time now\n\t3. exit");
+            selection = scanner.next();
+            switch (selection) {
+                case "1" -> System.out.println("Date now: " + getDateNow());
+                case "2" -> System.out.println("Time now: " + getTimeNow());
+            }
+        }while (!selection.equals("3"));
     }
 
     private static String getDateNow(){
